c311eb0927ce47b6fb11da6b79f823f180087368
Jane <spring1981@mail.ru>
2022-08-04 22:09:44 +0300

    task36 updated

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -1,4 +1,50 @@
 ﻿// Задача 36:
 // Задайте массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 
+int[] CreateRandomArray(int size, int min, int max)
+{
+    int[] array = new int [10];
 
+    for (int i = 0; i < 10; i++)
+
+        array[i] = new Random().Next(min, max + 1);
+
+        return array;
+}
+
+    
+
+void PrintArray(int [] array)
+{
+    
+    for(int i = 0; i < array.Length; i++)
+    Console.Write($"{array[i]} "); 
+
+}
+
+    Console.Write("[");
+
+int SumOddDigitals (int[] array)
+{
+    int sum = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0) sum += array[i];
+    }
+
+    return sum;
+
+    Console.Write("]");
+}
+
+    
+
+int [] elements = CreateRandomArray(10, 1, 100);
+PrintArray(elements);
+
+Console.WriteLine();
+
+int number = SumOddDigitals(elements);
+
+Console.Write($"Сумма элементов на нечетных позициях: {number}");
